d453b47ad45eec6297b5450371f663bda6588fa7
Artem Teslenko <ates@ipv6.dp.ua>
2024-11-15 22:11:37 +0200

    Add fallback CPU detection

--- src/osn_cpu.erl
+++ src/osn_cpu.erl
@@ -1,20 +1,24 @@
 -module(osn_cpu).
 
 -export([detect/0]).
+-export([fallback_detect/0]).
 
 detect() ->
     {0, Output} = osn_system_shell:exec("lscpu -J"),
     #{<<"lscpu">> := Data} = jsx:decode(Output),
-    {VendorId, Info} = vendor_id(Data),
-    Model = 
-        case get_field(<<"Model name:">>, Data) of
-            undefined ->
-                {ok, Value, _} = get_field(<<"Model name:">>, maps:get(<<"children">>, Info)),
-                Value;
-            {ok, Value, _} -> Value
-        end,
-    persistent_term:put({config, cpu_vendor_id}, VendorId),
-    persistent_term:put({config, cpu_model_name}, Model).
+    case vendor_id(Data) of
+        undefined ->
+            fallback_detect();
+        {VendorId, Info} ->
+            Model = case get_field(<<"Model name:">>, Data) of
+                undefined ->
+                    {ok, Value, _} = get_field(<<"Model name:">>, maps:get(<<"children">>, Info)),
+                    Value;
+                {ok, Value, _} -> Value
+            end,
+        persistent_term:put({config, cpu_vendor_id}, VendorId),
+        persistent_term:put({config, cpu_model_name}, Model)
+    end.
 
 get_field(Field, [#{<<"field">> := Field} = Entry | _Rest]) ->
     {ok, maps:get(<<"data">>, Entry), Entry};
@@ -24,7 +28,7 @@ get_field(_Field, []) -> undefined.
 
 vendor_id(Data) ->
     case get_field(<<"Vendor ID:">>, Data) of
-        undefined -> <<"Unknown">>;
+        undefined -> undefined;
         {ok, VendorId, Entry} ->
             {format_vendor_id(VendorId), Entry}
     end.
@@ -32,3 +36,18 @@ vendor_id(Data) ->
 format_vendor_id(<<"GenuineIntel">>) -> <<"INTEL">>;
 format_vendor_id(<<"AuthenticAMD">>) -> <<"AMD">>;
 format_vendor_id(VendorId) -> VendorId.
+
+fallback_detect() ->
+     {ok, CPUInfo} = file:read_file("/proc/cpuinfo"),
+     Data = binary:split(CPUInfo, <<"\n">>, [global, trim_all]),
+     persistent_term:put({config, cpu_vendor_id}, fallback_vendor_id(Data)),
+     persistent_term:put({config, cpu_model_name}, fallback_model_name(Data)).
+
+fallback_vendor_id([<<"vendor_id\t: ", VendorId/binary>> | _Rest]) -> format_vendor_id(VendorId);
+fallback_vendor_id([_Param | Rest]) -> fallback_vendor_id(Rest);
+fallback_vendor_id([]) -> undefined.
+
+fallback_model_name([<<"model name\t: ", Model/binary>> | _Rest]) -> Model;
+fallback_model_name([<<"Model\t\t: ", Model/binary>> | _Rest]) -> Model;
+fallback_model_name([_Param | Rest]) -> fallback_model_name(Rest);
+fallback_model_name([]) -> undefined.
